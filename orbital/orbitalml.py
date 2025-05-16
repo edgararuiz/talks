1a8a83d0cbab7676a12b174ffb035292063be375
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-05-16 15:39:01 -0500

    Adds initial DB connection

---
+++ orbital/orbitalml.py
@@ -0,0 +1,21 @@
+#pip install databricks-sql-connector
+
+import os
+from dotenv import load_dotenv
+from databricks import sql
+import pandas as pd
+load_dotenv()
+
+host = os.getenv("DATABRICKS_HOST")
+token = os.getenv("DATABRICKS_TOKEN")
+schema = "end-to-end"
+catalog = "sol_eng_demo_nickp"
+http_path = "/sql/1.0/warehouses/b71952ebceb705ce"
+
+con = sql.connect(host, http_path, token, catalog = catalog, schema = schema)
+con_cursor = con.cursor()
+con_cursor.execute("select * from loans_full_schema limit 10")
+column_names = [desc[0] for desc in con_cursor.description]
+res = con_cursor.fetchall()
+df = pd.DataFrame(res, columns=column_names)
+df
